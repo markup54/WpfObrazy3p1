e2580811f64ed3cc5cd2f892f96fa8364b93dfc4
markup54 <marzenakupka@tlen.pl>
2024-11-29 11:39:00 +0100

    Dodaj pliki projektów.

---
+++ WpfObrazy3p1/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfObrazy3p1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Obraz> obrazy = new List<Obraz>();
+        int aktualny = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            obrazy.Add(new Obraz("rys1.jpg"));
+            obrazy.Add(new Obraz("rys2.jpg"));
+            obrazy.Add(new Obraz("rys3.jpg"));
+            obrazy.Add(new Obraz("rys4.jpg"));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            aktualny--;
+            if (aktualny < 0)
+            {
+                aktualny = obrazy.Count-1  ;
+            }
+            wyswietlObraz(aktualny);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            aktualny++;
+            if(aktualny == obrazy.Count)
+            {
+                aktualny = 0;
+            }
+            wyswietlObraz(aktualny);
+        }
+        private void wyswietlObraz(int i)
+        {
+            image.Source = new BitmapImage(new Uri(obrazy[i].UrlObrazka, UriKind.Relative ));
+            liczbapolubienTextBlock.Text = obrazy[i].LicznikPobran.ToString();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            obrazy[aktualny].LicznikPobran++;
+            wyswietlObraz(aktualny);
+        }
+    }
+}
---
+++ WpfObrazy3p1/Obraz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfObrazy3p1
+{
+    public class Obraz
+    {
+        public string UrlObrazka { get; set; }
+        public int LicznikPobran { get; set; }
+
+        public Obraz(string urlObrazka)
+        {
+            UrlObrazka = urlObrazka;
+            LicznikPobran = 0;
+        }
+    }
+}
